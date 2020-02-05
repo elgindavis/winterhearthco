537a2a958837222839c730ebf243b4c15f963734
Elgin Davis <24661956+elgindavis@users.noreply.github.com>
2020-02-05 14:39:05 -0500

    added newsletter signup without error handling

--- redesign/custom-elements/footer.js
+++ redesign/custom-elements/footer.js
@@ -31,6 +31,9 @@ customElements.define('wh-footer', class extends WHFooter {
 Copyright &copy;2020 All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
 <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
                             </div>
+                            <div style="padding: 40px 0;">
+                                <button style="background: black; width: 165px; height: 30px; border-radius: 3px; border: 1px solid black; font-family: Roboto, Arial; font-size: 10px; color: white; font-weight: 600; box-sizing: border-box; cursor: pointer; padding: 0; outline: none;" onclick="displayPreferenceModal()">Manage Cookie Preferences</button>
+                            </div>
                         </div>
                         <div class="col-lg-5 col-md-6 col-sm-6 text-white">
                            <div class="single-footer-widget">
@@ -58,7 +61,7 @@ Copyright &copy;2020 All rights reserved | This template is made with <i class="
                                         </div> -->
                                         <div class="mc-field-group">
                                             <label class="form-label" for="mce-FNAME">First Name  <span class="asterisk" style="margin-right:24px;"></span>
-                                        </label>
+                                            </label>
                                             <input class="input-fields" type="text" value="" name="FNAME" class="required" id="mce-FNAME">
                                         </div>
                                         <div class="mc-field-group ">
@@ -69,17 +72,22 @@ Copyright &copy;2020 All rights reserved | This template is made with <i class="
                                         <div id="mce-responses" class="clear">
                                             <div class="response" id="mce-error-response" style="display:none"></div>
                                             <div class="response" id="mce-success-response" style="display:none">
+                                                You're all set! Check your inbox for your first email :)
                                             </div>
                                             <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_82935dc1a750f772912d12316_31c5d4e468" tabindex="-1" value="">
                                             </div>
                                             <div class="clear">
-                                                <button style="" type="submit" value="SUBSCRIBE" name="subscribe" id="mc-embedded-subscribe" class="primary-btn">JOIN THE TEAM
+                                                <button style="margin-bottom: 40px;" type="submit" value="SUBSCRIBE" name="subscribe" id="mc-embedded-subscribe" class="primary-btn">JOIN THE TEAM
                                                 </button>
                                             </div>
+
                                         </div> 
                                     </div>
                                 </form>
                             </div>
+                            <div class="response" id="signup-success" style="display:none">
+                                                You're all set! Check your inbox for your first email :)
+                                            </div>
                         </div>
                         <div class="col-lg-2 col-md-6 col-sm-6 social-widget">
                             <div class="single-footer-widget">
@@ -97,13 +105,12 @@ Copyright &copy;2020 All rights reserved | This template is made with <i class="
                         </div>
                     </div>
                 </div>
-                <div style="padding-top: 20px;">
-                    <button style="background: black; width: 165px; height: 30px; border-radius: 3px; border: 1px solid black; font-family: Roboto, Arial; font-size: 10px; color: white; font-weight: 600; box-sizing: border-box; cursor: pointer; padding: 0; outline: none;" onclick="displayPreferenceModal()">Manage Cookie Preferences</button>
-                </div>
+                
             </footer>
+                
             <script type='text/javascript' src='https://s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[0]='EMAIL';ftypes[0]='email';fnames[3]='ADDRESS';ftypes[3]='address';fnames[4]='PHONE';ftypes[4]='phone';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+
             `;
   }
 });
 
-// console.log("Welcome to Winter Hearth Studios! I see you know your way around a console ;)");
