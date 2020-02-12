594fd178145fc6f9a616cdb5eec76030230617ce
Elgin Davis <24661956+elgindavis@users.noreply.github.com>
2020-02-12 13:23:11 -0500

    add back linear icons css

--- custom-elements/footer.js
+++ custom-elements/footer.js
@@ -28,7 +28,7 @@ customElements.define('wh-footer', class extends WHFooter {
                                     Change the heart, change the world.
                                 </p>
                                 <p class="footer-text"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy;2020 All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+Copyright &copy;2020 All rights reserved | This template is made by <a href="https://colorlib.com" target="_blank">Colorlib</a>
 <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
                             </div>
                             <div style="padding: 40px 0;">
