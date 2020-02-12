a91dc5ab02ade9a57267b7de52bae70a4b4a4a34
Elgin Davis <24661956+elgindavis@users.noreply.github.com>
2020-02-12 11:57:57 -0500

    trying jpg sizes

--- custom-elements/header.js
+++ custom-elements/header.js
@@ -26,7 +26,7 @@ customElements.define('wh-header', class extends WHHeader {
 		    <div class="container main-menu">
 		    	<div class="row align-items-center justify-content-between d-flex">
 			      <div id="logo">
-			        <a href="${this.isNotRootDirectory ? this.changeLinkDirectory : ""}."><img style="max-height: 56px;" src="${this.isNotRootDirectory ? this.changeLinkDirectory : ""}img/logo-200p.png" alt="" title="" /></a>
+			        <a href="${this.isNotRootDirectory ? this.changeLinkDirectory : ""}."><img style="max-height: 56px;" src="${this.isNotRootDirectory ? this.changeLinkDirectory : ""}img/logo-200p.jpg" alt="" title="" /></a>
 			      </div>
 			      <nav id="nav-menu-container">
 			        <ul class="nav-menu">
