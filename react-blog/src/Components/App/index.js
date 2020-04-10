ee68abafcba255e4868765c6a8b8fae18852e7b4
Elgin Davis <24661956+elgindavis@users.noreply.github.com>
2020-04-10 00:16:36 -0400

    Time to set up Next.js

--- react-blog/src/Components/App/index.js
+++ react-blog/src/Components/App/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-multi-str */
 import React from 'react';
 import Header from '../Header';
 import HomeBanner from '../HomeBanner';
@@ -7,6 +8,15 @@ import ButtonSection from '../ButtonSection';
 import HomeInfoContainer from '../../Containers/HomeInfoContainer';
 
 import './App.css';
+
+const homeSectionText = [
+  "We're a creative studio dedicated to sparking meaningful change around mental and emotional health. We fully \
+      believe that if you can change the heart, you can change the world.",
+  "We believe in the power of storytelling as a central unifier in our shared human experience. From web \
+      comics to video games, podcasts to videos, blog posts and beyond— we aim to inspire, educate, and enrich \
+      people all over the world with the tools they need to become more mentally and emotionally powerful."
+];
+
 
 const App = (props) => {
   return (
@@ -23,18 +33,14 @@ const App = (props) => {
       />
       <HomeInfoContainer
         title="What fuels us"
-        text=" We're a creative studio dedicated to sparking meaningful change around mental and emotional health. We fully
-              believe that if you can change the heart, you can change the world."
+        text={homeSectionText[0]}
         buttonText="Read Our Story"
         imgURL="../../img/aubbie-knight.jpg"        
       />
       <HomeInfoContainer
         classes="pt-60 pb-120"
         title="What we do"
-        text="We believe in the power of storytelling as a central unifier in our shared human experience. From web
-              comics to video games, podcasts to videos, blog posts and beyond— we aim to inspire, educate, and enrich
-              people all over the world with the tools they need to become more mentally and emotionally powerful."
-        buttonText="See latest posts"
+        text={homeSectionText[1]}
         imgURL="../../img/aubbie-writing.png"       
       />
       <Footer/>
