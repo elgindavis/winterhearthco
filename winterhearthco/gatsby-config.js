3f1e60a5b090fac304096b8d40f0112f3c04135e
elgindavis <elgindavis9@gmail.com>
2020-04-26 12:23:00 -0400

    Set up Gatsby for dynamic blog page creation

--- winterhearthco/gatsby-config.js
+++ winterhearthco/gatsby-config.js
@@ -11,5 +11,29 @@ module.exports = {
       twitter: `winterhearthco`,
     },
   },
- 
+  plugins: [
+    `gatsby-transformer-remark`,
+    `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#6b37bf`,
+        theme_color: `#6b37bf`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `src/img/fav.png`, // This path is relative to the root of the site.
+      },
+    }
+  ],
 }
