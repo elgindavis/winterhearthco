9d72bb2d1670f85134c5bc7f9c45b9f2fcc50af6
elgindavis <elgindavis9@gmail.com>
2020-05-25 17:38:29 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,6 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        // replace "UA-XXXXXXXXX-X" with your own Tracking ID
         trackingId: "UA-155762301-1",
       },
     },
